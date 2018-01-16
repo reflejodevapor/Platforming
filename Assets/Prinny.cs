8eaaea549798940e7ca039854ae41ef1b8799a27
adansinacento <sdgfd>
2018-01-16 13:12:17 -0600

    movimiento básico
    ya se mueve, salta y eso básico

---
+++ Assets/Prinny.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Prinny : MonoBehaviour 
+{
+	[SerializeField]
+	private float potenciaSalto = 5;
+	[SerializeField]
+	private float velocidad = 5;
+	private Rigidbody2D rb;
+	public Transform GOSalto;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D> (); //inicializamos la variable del RigidBody
+		velocidad = 5;
+		potenciaSalto = 6;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float dir = Input.GetAxis ("Horizontal");
+		if (Input.GetKeyDown (KeyCode.Space) && Physics2D.OverlapPoint(GOSalto.position)) 
+		{
+			rb.AddForce (Vector2.up * potenciaSalto, ForceMode2D.Impulse);
+		}
+		transform.Translate (dir * velocidad * Time.deltaTime, 0, 0);
+	}
+}
