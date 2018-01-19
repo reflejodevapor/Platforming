a81bd4d651c2fcda6d51b2fed812fa5eec00a600
adansinacento <sdgfd>
2018-01-19 15:05:35 -0600

    avance del 19/ene
    patatas

--- Assets/Prinny.cs
+++ Assets/Prinny.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class Prinny : MonoBehaviour 
 {
 	[Header("Variables privadas de Prinny")]
@@ -32,15 +33,19 @@ public class Prinny : MonoBehaviour
 		float dir = Input.GetAxis ("Horizontal");
 		if (Input.GetKeyDown (KeyCode.Space)) 
 		{
-			if (dobleSalto || Physics2D.OverlapPoint (GOSalto.position)) 
+			if (dobleSalto || Physics2D.OverlapPoint (GOSalto.position)) // Manejador de saltos
 			{
+				rb.velocity = Vector2.zero;
 				rb.AddForce (Vector2.up * potenciaSalto, ForceMode2D.Impulse);
 				dobleSalto = false;
 			}
 
 		}
-		if (Physics2D.OverlapPoint (GOSalto.position))
+		if (Physics2D.OverlapPoint (GOSalto.position)) // cuando está tocando el piso activamos el salto doble
 			dobleSalto = true;
 		transform.Translate (dir * velocidad * Time.deltaTime, 0, 0);
+		Vector2 S = GetComponent<SpriteRenderer> ().bounds.size;
+		GetComponent<BoxCollider2D> ().size = S;
+		GetComponent<BoxCollider2D> ().offset;
 	}
 }
