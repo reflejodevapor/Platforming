41c61e8a5f909b1b02d4b40a809260acb3648af4
Rubén Ostos <reflejodevapor@gmail.com>
2018-01-24 12:29:40 -0600

    ya estoy haciendo el mapita

--- Assets/Prinny.cs
+++ Assets/Prinny.cs
@@ -30,7 +30,7 @@ public class Prinny : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		float dir = Input.GetAxis ("Horizontal");
+		
 		if (Input.GetKeyDown (KeyCode.Space)) 
 		{
 			if (dobleSalto || Physics2D.OverlapPoint (GOSalto.position)) // Manejador de saltos
@@ -43,9 +43,28 @@ public class Prinny : MonoBehaviour
 		}
 		if (Physics2D.OverlapPoint (GOSalto.position)) // cuando está tocando el piso activamos el salto doble
 			dobleSalto = true;
-		transform.Translate (dir * velocidad * Time.deltaTime, 0, 0);
+		
 		Vector2 S = GetComponent<SpriteRenderer> ().bounds.size;
 		GetComponent<BoxCollider2D> ().size = S;
-		GetComponent<BoxCollider2D> ().offset;
+//		GetComponent<BoxCollider2D> ().offset;
 	}
+
+	void FixedUpdate()
+	{
+		float dir = Input.GetAxis ("Horizontal");
+		transform.Translate (dir * velocidad * Time.deltaTime, 0, 0);
+	}
+
+	void OnCollisionEnter2D(Collision2D col) 
+	{
+		if (col.gameObject.CompareTag("Walls"))
+			rb.velocity = Vector2.zero;
+	}
+
+	void OnCollisionStay2D(Collision2D col) 
+	{
+		if (col.gameObject.CompareTag("Walls"))
+			rb.velocity = Vector2.zero;
+	}
+
 }
