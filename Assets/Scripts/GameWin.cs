631b1636c5404368de6b7e1c2d2c5e2acbf56691
adansinacento <sdgfd>
2018-02-09 15:41:14 -0600

    finish

---
+++ Assets/Scripts/GameWin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameWin : MonoBehaviour 
+{
+
+	public Image win;
+	public bool ganastes = false;
+
+	void Start()
+	{
+		win.enabled = false;
+	}
+
+	void Update()
+	{
+		if (ganastes) 
+		{
+			if (Time.timeScale > 0.2f)
+				Time.timeScale -= 0.01f;
+			else if (Time.timeScale < 0.2f)
+				Time.timeScale = 0f;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		win.enabled = true;
+		ganastes = true;
+	}
+}
